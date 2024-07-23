1419fc497cd4c1106ba38c24082b06db4e82790c
Zhwee <eternallingfice@gmail.com>
2024-07-23 13:11:44 +0800

    first commit

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+# Clone the repository
+git clone https://github.com/Zhwee/jenkins-php-selenium-test-two.git
+
+# Change directory to the cloned repository
+cd jenkins-php-selenium-test-two
+
+# Run the Docker container
+
+docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
